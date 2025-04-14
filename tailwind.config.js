bc4538e24b24ea5068893c8090585553c460e03f
leejungmin1649 <trustman7278@gmail.com>
2025-04-14 22:49:57 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  darkMode: 'class',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // 반드시 있어야 함
   theme: {
     extend: {},
   },
