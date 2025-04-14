8a19bd4c0e3a9e66a6f8d114cbf9efeb54c30e16
leejungmin1649 <trustman7278@gmail.com>
2025-04-14 20:33:31 +0900

    Update NanumGothic.js

--- components/NanumGothic.js
+++ components/NanumGothic.js
@@ -1,3 +1,3 @@
-/* NanumGothic.js (jsPDF font file placeholder)
-   실제 사용 시 fontconverter로 변환된 .js 내용으로 교체 필요 */
-var NanumGothic = "AAEAAAALAIAAAwAwT1MvMg8SXTkAA...";
+// NanumGothic.js
+const NanumGothic = "AAEAAAALAIAAAwAwT1MvMg8SXTkAA..."; // 전체 base64 문자열
+export default NanumGothic;
