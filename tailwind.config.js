243de03a6a1b02e7d1741eba439934673eed87e3
leejungmin1649 <trustman7278@gmail.com>
2025-04-14 22:59:53 +0900

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // 반드시 있어야 함
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
