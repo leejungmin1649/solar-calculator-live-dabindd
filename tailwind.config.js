925d8ffb834f538203af79af78e0b3c341da810f
leejungmin1649 <trustman7278@gmail.com>
2025-04-15 22:29:40 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
-// 반드시 포함되어야 함
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // ✅ 다크모드를 클래스 기반으로 사용
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',     // 페이지 폴더 내부
+    './components/**/*.{js,ts,jsx,tsx}' // 컴포넌트 폴더 내부
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: {
+          400: '#34D399',
+        },
+        violet: {
+          400: '#818CF8',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
