e314b935a4be197d670e31a8ff0b6966604b1888
leejungmin1649 <trustman7278@gmail.com>
2025-04-15 23:21:09 +0900

    Update NanumGothic_full.js

--- components/NanumGothic_full.js
+++ components/NanumGothic_full.js
@@ -1,5 +1,3 @@
-import NanumGothic from './NanumGothic_full';
-
-doc.addFileToVFS('NanumGothic.ttf', NanumGothic);
-doc.addFont('NanumGothic.ttf', 'NanumGothic', 'normal');
-doc.setFont('NanumGothic');
+// components/NanumGothic_full.js
+const NanumGothic = "data:font/truetype;charset=utf-8;base64,AAEAAAALAIAAA..."; // 실제 base64로 대체 필요
+export default NanumGothic;
