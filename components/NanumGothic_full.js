9627fc8ab1d716b9acad065bc4607c02f1912c63
leejungmin1649 <trustman7278@gmail.com>
2025-04-15 23:40:49 +0900

    Update NanumGothic_full.js

--- components/NanumGothic_full.js
+++ components/NanumGothic_full.js
@@ -1,3 +1,2 @@
-// components/NanumGothic_full.js
-const NanumGothic = "data:font/truetype;charset=utf-8;base64,AAEAAAALAIAAA..."; // 실제 base64로 대체 필요
+const NanumGothic = "data:font/truetype;charset=utf-8;base64,AAEAAAALAIAAA..."; // 실제 base64 데이터
 export default NanumGothic;
