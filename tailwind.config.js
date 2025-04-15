6b09a19f269e5c529adb3e673ad42032ef7e5e72
leejungmin1649 <trustman7278@gmail.com>
2025-04-15 22:20:53 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+// 반드시 포함되어야 함
 module.exports = {
   darkMode: 'class',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
