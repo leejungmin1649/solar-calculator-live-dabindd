35027d5f34a680fb7ddb8703768a53bf2f964b2a
leejungmin1649 <trustman7278@gmail.com>
2025-05-07 23:27:05 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,16 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Kakao SDK: 페이지가 인터랙티브 해진 후 한 번만 로드 */}
+      {/* Kakao SDK 로드: 페이지가 인터랙티브해진 후 한 번만 */}
       <Script
         src="https://developers.kakao.com/sdk/js/kakao.min.js"
         strategy="afterInteractive"
+        onLoad={() => {
+          if (window.Kakao && !window.Kakao.isInitialized()) {
+            window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+            console.log('Kakao SDK initialized with key:', process.env.NEXT_PUBLIC_KAKAO_KEY);
+          }
+        }}
       />
       <Component {...pageProps} />
     </>
