e262d88a3eb9b06c1cb381df63813710ec00e297
leejungmin1649 <trustman7278@gmail.com>
2025-05-07 23:23:50 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+// pages/_app.js
 import '../styles/globals.css';
 import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Kakao SDK 로드: 페이지가 인터랙티브해진 후 */}
+      {/* Kakao SDK: 페이지가 인터랙티브 해진 후 한 번만 로드 */}
       <Script
         src="https://developers.kakao.com/sdk/js/kakao.min.js"
         strategy="afterInteractive"
