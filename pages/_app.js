2891d38140209ad85c641bba638df8c5ae69ab4d
leejungmin1649 <trustman7278@gmail.com>
2025-05-07 22:33:35 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Kakao SDK 로드: 페이지가 인터랙티브해진 후 */}
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.min.js"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
