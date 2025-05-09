27e89c396e4a9845772c46759b94fabf169e1a7f
leejungmin1649 <trustman7278@gmail.com>
2025-05-09 23:00:30 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         strategy="afterInteractive"
         onLoad={() => {
           if (window.Kakao && !window.Kakao.isInitialized()) {
-            window.Kakao.init('f5b4cfb16c5b2f8e213a1549a009307a');
+            window.Kakao.init('a02ad11689f9d4b1ffd2a081c08d5270');
             console.log('✅ Kakao SDK initialized');
           }
         }}
